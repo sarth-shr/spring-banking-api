78b666b3dfe2072ad258012a6d6143d24dd6cddc
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2024-07-25 11:07:37 +0545

    Set JWT expiration time to one month

--- src/main/java/com/example/foneproject/util/JwtUtils.java
+++ src/main/java/com/example/foneproject/util/JwtUtils.java
@@ -32,7 +32,7 @@ public class JwtUtils {
                 .and()
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 30))
                 .signWith(getSignInKey())
                 .compact();
     }
